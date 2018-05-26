5d5868f5ceaa837b9568487bfdd9278e34cdd298
GaneshKumar Gunju <ganeshkumargunju@MacBook-Pro.local>
2018-05-26 10:29:00 +0530

    First commit

--- UserLocation/ViewController.swift
+++ UserLocation/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import MapKit
+import CoreLocation
+import GoogleMaps
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+//        GMSServices.provideAPIKey("AIzaSyCFO0ufqj8Yr8wkO7KgdKxa3OhrMFSt884")
+//        let camera = GMSCameraPosition.camera(withLatitude: 14.412405, longitude: 78.228370, zoom: 12.0)
+//        //let mapView = GMSMapView.map(withFrame: 0, camera: camera)
+//        let currentlocation = CLLocationCoordinate2DMake(14.412405, 78.228370)
+//        let marker = GMSMarker(position: currentlocation)
+//        marker.title = "pulivendula"
+//        marker.snippet = "kadapa"
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
 
+        let location = locations[0]
+        let center = location.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+
+
+        mapView.setRegion(region, animated: true)
+        mapView.showsUserLocation = true
+
+            }
+    //AIzaSyCFO0ufqj8Yr8wkO7KgdKxa3OhrMFSt884
 
 }
-
